7677574a9b21f63bc44c18ccab7b72dee64841fc
Brian Bigdelle <bb586@cornell.edu>
2015-06-01 09:43:49 -0400

    Building todo list application using rails 4.0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Odot::Application.config.secret_key_base = 'ac0340554f6e3a98d80800fb0b56cd3bfe7efa145e3920b967eac50bb6241bdd297ce6c576c78174d9ee95154f704ba24c2f7c4e7ebcf5ac461f77585a7521a3'
